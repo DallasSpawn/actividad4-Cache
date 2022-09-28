e09bf78b89abe7020e7344e0126b0b53f14f03ab
Medrano Brian <67834746+DallasSpawn@users.noreply.github.com>
2022-09-27 23:02:24 -0500

    feat: Final commit

--- sw.js
+++ sw.js
@@ -13,7 +13,8 @@ self.addEventListener('install', (event)=>{
                 './css/apps.css',
                 './js/app.js',
                 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
-                'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js'
+                'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
+                'https://code.jquery.com/jquery-3.6.1.min.js'
             ]
         );
     })
